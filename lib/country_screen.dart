ff2f75e8dced2018adafb21351a9d4513c95805a
ShopyDan <daniyarkyzyaisholpan@gmail.com>
2024-03-31 17:08:58 +0500

    in eng

--- lib/country_screen.dart
+++ lib/country_screen.dart
@@ -42,9 +42,9 @@ class _CountryScreenState extends State<CountryScreen> {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(backgroundColor: Colors.blue,
-          title: Text('Страны',
+          title: Text('Country',
               style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 22,
                   fontWeight: FontWeight.bold,
                   color: Colors.white)),),
 
@@ -57,7 +57,7 @@ class _CountryScreenState extends State<CountryScreen> {
                     ExpansionTile(
                       leading: CircleAvatar(backgroundImage: NetworkImage(countries[index].flags!.png!),),
                       title: Text(countries[index].name!.common!),
-                      children: [ Text('Столица: ' + countries[index].capital!.first)],
+                      children: [ Text('Capital: ' + countries[index].capital!.first)],
                       
                     );
                     
